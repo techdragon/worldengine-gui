63fcaea0eb64a9d84a4695b6b04e01fe26ce6d9b
Samuel Bishop <sam@psyx.co>
2021-08-21 11:54:35 +0800

    Update WatermapView

--- worldengine_gui/views/WatermapView.py
+++ worldengine_gui/views/WatermapView.py
@@ -1,14 +1,14 @@
 from PyQt5 import QtGui
 
-import worldengine as we
+from worldengine.model.world import World
 import worldengine.draw
 
-class WatermapView(object):
 
+class WatermapView(object):
     @staticmethod
     def is_applicable(world):
         return world.has_watermap()
 
     @staticmethod
-    def draw(world, canvas):
-        we.draw.draw_ocean(world.layers['ocean'].data, canvas)
+    def draw(world: World, canvas):
+        worldengine.draw.draw_ocean(world.layers['ocean'].data, canvas)
